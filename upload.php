7fe253f3d0a85c30d353d1ccaad45ff0cc172019
OwenHUANG <huangzhengyuan1993@gmail.com>
2014-03-26 11:45:54 +0800

    change the datadir

--- upload.php
+++ upload.php
@@ -1,12 +1,12 @@
 <?php
 	$fn = (isset($_SERVER['HTTP_FILE_NAME']) ? $_SERVER['HTTP_FILE_NAME'] : false);
 	
-	$upload_dir = (isset($_ENV['OPENSHIFT_DATA_DIR']) ? $_ENV['OPENSHIFT_DATA_DIR'] : "/var/www");
+	$upload_dir = (isset($_ENV['OPENSHIFT_DATA_DIR']) ? $_ENV['OPENSHIFT_DATA_DIR'] : "/var/www/");
 	if ($fn && $upload_dir)
 	{
 		$raw_data = file_get_contents('php://input');
 		$data = base64_decode($raw_data);
-		$result = file_put_contents($upload_dir . '/' . $fn, $data);
+		$result = file_put_contents($upload_dir . '' . $fn, $data);
 		if ($data)
 		{
 			echo "uploaded!" . $upload_dir . '/' . $fn;
